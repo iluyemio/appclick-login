6bc799ee92d463bd406f731a16dae79bb27533b7
iluyemio <iluyemio@gmail.com>
2025-07-13 23:58:47 +0100

    new commit

---
+++ appclick.js
@@ -0,0 +1,18 @@
+const userEmail = document.querySelector("#email");
+const myPassword = document.querySelector("#password");
+const signIn_btn = document.querySelector("#signIn");
+
+
+input_email = "appclick@gmail.com";
+input_password = "1234AppClick"
+
+signIn_btn.addEventListener("click", function(){
+    const userValue = userEmail.value;
+    const passwordValue = myPassword.value;
+
+    if (userValue === input_email && passwordValue === input_password){
+        alert("Welcome! Your have successfully login");
+    } else {
+        alert("Incorrect login credentials");
+    }
+});
